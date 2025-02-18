08142550ee4a0176c651cab31496e72daa4693d4
rdlynx19 <pushkardave.vnit@gmail.com>
2025-02-17 20:23:30 -0600

    setting up pi_mapping for urdf edits

--- remote_rtabmap/setup.py
+++ remote_rtabmap/setup.py
@@ -14,6 +14,7 @@ setup(
                                    'launch/rt.launch.py',
                                    'launch/remote.launch.py',
                                    'launch/custom_rgbd.launch.py',
+                                   'launch/stereo_inertial_node.launch.py',
                                    'config/map.rviz',
                                    'urdf/base_descr.urdf.xacro',
                                    'urdf/depthai_descr.urdf.xacro',
